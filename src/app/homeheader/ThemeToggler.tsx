5b8f6edf42e3db93cea130451a4cfe441c6615e4
chengivan <chengoleivan@gmail.com>
2025-03-30 20:41:49 +0300

    Now using the shadcn Buttoon cvomponent to wrap the theme toggler icons

--- src/app/homeheader/ThemeToggler.tsx
+++ src/app/homeheader/ThemeToggler.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import { FaMoon } from "react-icons/fa6";
 import { IoSunny } from "react-icons/io5";
@@ -7,12 +8,13 @@ import { IoSunny } from "react-icons/io5";
 export default function HeaderThemeToggler() {
   const { theme, setTheme } = useTheme();
   return (
-    <button
+    <Button
+      size="icon"
       className="flex justify-center items-center duration-200 border-none bg-transparent outline-0 rounded-full hover:cursor-pointer active:outline-0"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
       <FaMoon size={12} className="absolute h-12 w-12 -rotate-21 duration-300 scale-100 text-darkmode-bg-color dark:-rotate-0 dark:scale-0" />
       <IoSunny size={12} className="absolute h-12 w-12 -rotate-90 scale-0 dark:-rotate-22 duration-300 dark:scale-100" />
-    </button>
+    </Button>
   );
 }
