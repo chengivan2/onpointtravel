761c2128ddf5038b32a0fe05b208b91430e750f7
chengivan <chengoleivan@gmail.com>
2025-03-30 12:48:40 +0300

    Added the fonts.ts initial code

---
+++ src/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Alegreya_Sans } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+export const alegreyaSans = Alegreya_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['100', '300', '400', '500', '700', '800', '900']
+})
