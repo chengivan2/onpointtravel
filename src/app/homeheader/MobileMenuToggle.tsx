69cc38a7ae1ab228940193bdf12b0570f3571a76
chengivan <chengoleivan@gmail.com>
2025-03-30 18:50:59 +0300

    Fixed MobileMenuToggle

--- src/app/homeheader/MobileMenuToggle.tsx
+++ src/app/homeheader/MobileMenuToggle.tsx
@@ -1,25 +1,16 @@
-// components/Header/MobileMenuToggle.tsx
-import React from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
+"use client"
 
+import { FaBars, FaTimes } from 'react-icons/fa';
 interface MobileMenuToggleProps {
   isOpen: boolean;
-  toggleMenu: () => void;
 }
 
-const MobileMenuToggle: React.FC<MobileMenuToggleProps> = ({ isOpen, toggleMenu }) => {
+const MobileMenuToggle = ({ isOpen }: MobileMenuToggleProps) => {
+ 
   return (
-    // Shown only on small screens
-    <div className="md:hidden">
-      <button
-        onClick={toggleMenu}
-        aria-label={isOpen ? 'Close menu' : 'Open menu'}
-        aria-expanded={isOpen}
-        className="p-2 rounded-md text-lightmode-headertext-color hover:text-lightmode-headertext-hover-color dark:text-darkmode-headertext-color dark:hover:text-darkmode-headertext-hover-color transition-colors focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lightmode-heading-color dark:focus:ring-darkmode-heading-color"
-      >
-        {isOpen ? <FaTimes className="w-6 h-6" /> : <FaBars className="w-6 h-6" />}
-      </button>
-    </div>
+    <>
+      {isOpen ? <FaTimes className="w-6 h-6" /> : <FaBars className="w-6 h-6" />}
+    </>
   );
 };
 
