dc44b92071cb703bf82e9dda02ac456bec88d3d8
chengivan <chengoleivan@gmail.com>
2025-03-30 20:18:43 +0300

    Reduced the sizes of the theme toggler icons

--- src/app/homeheader/ThemeToggler.tsx
+++ src/app/homeheader/ThemeToggler.tsx
@@ -9,8 +9,8 @@ export default function HeaderThemeToggler() {
       className="flex justify-center items-center duration-200 border-none bg-transparent outline-0 rounded-full hover:cursor-pointer active:outline-0"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <FaMoon className="absolute text-2xl h-12 w-12 -rotate-21 duration-300 scale-100 text-darkmode-bg-color dark:-rotate-0 dark:scale-0" />
-      <IoSunny className="absolute text-2xl h-12 w-12 -rotate-90 scale-0 dark:-rotate-22 duration-300 dark:scale-100" />
+      <FaMoon className="absolute text-sm h-12 w-12 -rotate-21 duration-300 scale-100 text-darkmode-bg-color dark:-rotate-0 dark:scale-0" />
+      <IoSunny className="absolute text-sm h-12 w-12 -rotate-90 scale-0 dark:-rotate-22 duration-300 dark:scale-100" />
     </button>
   );
 }
