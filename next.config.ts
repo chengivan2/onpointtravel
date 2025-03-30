8298cdb88d828f7d94b9c7148f9f36b19a3eee45
chengivan <chengoleivan@gmail.com>
2025-03-31 00:04:46 +0300

    Configured the pathname option in the image property of the next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
-        pathname: '',
+        pathname: '/**',
         search: '',
       },
     ],
