0ac245d493b138ef2f2080faab38eeedb22d3d80
chengivan <chengoleivan@gmail.com>
2025-03-30 20:51:06 +0300

    Modified styling

--- src/app/homeheader/ThemeToggler.tsx
+++ src/app/homeheader/ThemeToggler.tsx
@@ -10,11 +10,11 @@ export default function HeaderThemeToggler() {
   return (
     <Button
       size="icon"
-      className="flex justify-center items-center duration-200 border-none bg-transparent outline-0 rounded-full hover:cursor-pointer active:outline-0"
+      className="flex justify-center items-center duration-200 border-none bg-transparent hover:bg-transparent outline-0 rounded-full hover:cursor-pointer active:outline-0"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
       <FaMoon className="absolute h-12 w-12 -rotate-21 duration-300 scale-100 text-darkmode-bg-color dark:-rotate-0 dark:scale-0" />
-      <IoSunny className="absolute h-12 w-12 -rotate-90 scale-0 dark:-rotate-22 duration-300 dark:scale-100" />
+      <IoSunny className="absolute h-12 w-12 -rotate-90 scale-0 dark:text-lightmode-bg-color dark:-rotate-22 duration-300 dark:scale-100" />
     </Button>
   );
 }
