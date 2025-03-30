3c4dcee86997e1adf37977db7a49d87e653409d9
chengivan <chengoleivan@gmail.com>
2025-03-30 22:33:16 +0300

    Added FooterLogo

---
+++ src/app/footer/FooterLogo.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+
+export default function FooterLogo() {
+  return (
+    <div className="flex justify-between items-center">
+      <Image
+        width={100}
+        height={50}
+        alt="OnPoint footer logo"
+        src="/logos/onpointhflightmodelogo.png"
+        className="flex duration-300 dark:hidden"
+      />
+
+      <Image
+        width={100}
+        height={50}
+        alt="OnPoint footer logo"
+        src="/logos/onpointhfdarkmodelogo.png"
+        className="hidden duration-300 dark:flex"
+      />
+    </div>
+  );
+}
