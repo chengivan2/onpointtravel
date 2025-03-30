eed8f16f71335d146e5a69badb901f2671cebda8
chengivan <chengoleivan@gmail.com>
2025-03-30 22:29:44 +0300

    Added the footer menu

---
+++ src/app/footer/Menu.tsx
@@ -0,0 +1,97 @@
+import Link from "next/link";
+
+export default function FooterMenu() {
+  const links = [
+    {
+      group: "Top Destinations",
+      items: [
+        {
+          title: "Diani",
+          href: "#",
+        },
+        {
+          title: "Nairobi",
+          href: "#",
+        },
+        {
+          title: "Dubai",
+          href: "#",
+        },
+        {
+          title: "Masai Mara",
+          href: "#",
+        },
+        {
+          title: "Mombasa",
+          href: "#",
+        },
+        {
+          title: "Nakuru",
+          href: "#",
+        },
+      ],
+    },
+    {
+      group: "Company",
+      items: [
+        {
+          title: "About",
+          href: "#",
+        },
+        {
+          title: "Careers",
+          href: "#",
+        },
+        {
+          title: "Blog",
+          href: "#",
+        },
+        {
+          title: "Contact",
+          href: "#",
+        },
+        {
+          title: "Help",
+          href: "#",
+        },
+      ],
+    },
+    {
+      group: "Legal",
+      items: [
+        {
+          title: "TRA Licence",
+          href: "#",
+        },
+        {
+          title: "Privacy Policy",
+          href: "#",
+        },
+        {
+          title: "Terms & Conditions",
+          href: "#",
+        },
+      ],
+    },
+  ];
+  return (
+    <div className="grid grid-cols-2 gap-6 sm:grid-cols-4 md:col-span-3">
+      {links.map((link, index) => (
+        <div key={index} className="space-y-4 text-sm">
+          <span className="block font-bold">
+            <h2>{link.group}</h2>
+          </span>
+          {link.items.map((item, index) => (
+            <Link
+              key={index}
+              href={item.href}
+              className="font-extralight text-muted-foreground hover:text-primary block duration-150"
+            >
+              <span>{item.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
