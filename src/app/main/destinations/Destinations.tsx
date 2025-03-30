ff40f3c207cf6afb199c29c72557360366bdd591
chengivan <chengoleivan@gmail.com>
2025-03-30 23:38:53 +0300

    Created the Destinations section

---
+++ src/app/main/destinations/Destinations.tsx
@@ -0,0 +1,14 @@
+import DestinationCards from "./DestinationCards";
+
+export default function Destinations() {
+  return (
+    <section className="relative flex flex-col min-w-full">
+      <div className="flex relative min-w-full justify-center">
+        <h2 className="text-4xl font-black text-lightmode-heading-color dark:text-darkmode-heading-color">Top Destinations</h2>
+      </div>
+
+      <DestinationCards />
+
+    </section>
+  );
+}
