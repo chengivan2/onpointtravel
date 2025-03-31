47b0679b75594dddb7fa74fabd2c92ca3d0824ba
chengivan <chengoleivan@gmail.com>
2025-03-31 13:02:19 +0300

    Reverted back to Tailwind to test responsiveness on mobile again

--- src/app/main/bookingsteps/BookingStepsImage.tsx
+++ src/app/main/bookingsteps/BookingStepsImage.tsx
@@ -1,32 +1,31 @@
 "use client";
 
 import { TypeAnimation } from "react-type-animation";
-import "./bookingImage.css"
 
 export default function BookingStepsImage() {
   return (
     <div className="flex-1 w-full max-w-xl">
-      <div className="glassCard">
+      <div className="relative rounded-2xl shadow-xl border-green-100/30 dark:border-green-900/30 hover:shadow-xl p-8 border">
         <div className="space-y-6">
           <div className="flex justify-between items-start">
             <div>
-              <h3 className="glassCardTripTitle">
+              <h3 className="text-2xl font-bold text-green-800 dark:text-green-100">
                 Trip To Paris
               </h3>
 
               <TypeAnimation
-                sequence={["14-29 June 2025 | Booked by Sifa Joy", 900]}
+                sequence={["14-29 June 2025 | Booked by Sifa Joy", 2000]}
                 wrapper="span"
                 speed={50}
                 repeat={Infinity}
                 cursor={false}
-                className="glassCardTripInfo"
+                className="text-green-600 dark:text-green-300"
               />
             </div>
-            <span className="onGoingTripStatus">
-              <div className="onGoingTripDotContainer">
-                <div className="onGoingTripDotPulse"></div>
-                <div className="onGoingTripDot"></div>
+            <span className="relative flex items-center bg-green-100/50 dark:bg-green-900/50 text-green-700 dark:text-green-300 px-3 py-1 rounded-full text-sm">
+              <div className="relative mr-2">
+                <div className="absolute animate-pulse -inset-1 bg-gradient-to-r from-green-400/60 to-green-600/60 rounded-full blur-[2px]"></div>
+                <div className="relative w-2 h-2 rounded-full bg-green-700 dark:bg-green-300"></div>
               </div>
               Ongoing
             </span>
@@ -37,11 +36,11 @@ export default function BookingStepsImage() {
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-2">
               <div className="flex -space-x-3">
-                <div className="tripParticipantsFaceCard bg-[url(https://res.cloudinary.com/doqbnfais/image/upload/v1743234421/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/booking%20steps%20faces/booking-steps-face-image-1_lsbrim.jpg)]"></div>
-                <div className="tripParticipantsFaceCard bg-[url(https://res.cloudinary.com/doqbnfais/image/upload/v1743234420/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/booking%20steps%20faces/booking-steps-face-image-2_x5heop.jpg)]"></div>
-                <div className="tripParticipantsFaceCard bg-[url(https://res.cloudinary.com/doqbnfais/image/upload/v1743234420/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/booking%20steps%20faces/booking-steps-face-image-3_qhng3y.jpg)]"></div>
+                <div className="w-8 h-8 rounded-full bg-cover bg-[url(https://res.cloudinary.com/doqbnfais/image/upload/v1743234421/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/booking%20steps%20faces/booking-steps-face-image-1_lsbrim.jpg)] border-2 border-white dark:border-green-900"></div>
+                <div className="w-8 h-8 rounded-full bg-cover bg-[url(https://res.cloudinary.com/doqbnfais/image/upload/v1743234420/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/booking%20steps%20faces/booking-steps-face-image-2_x5heop.jpg)] border-2 border-white dark:border-green-900"></div>
+                <div className="w-8 h-8 rounded-full bg-cover bg-[url(https://res.cloudinary.com/doqbnfais/image/upload/v1743234420/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/booking%20steps%20faces/booking-steps-face-image-3_qhng3y.jpg)] border-2 border-white dark:border-green-900"></div>
               </div>
-              <span className="tripParticipants">
+              <span className="text-green-600 dark:text-green-300 text-sm">
                 5 people going
               </span>
             </div>
