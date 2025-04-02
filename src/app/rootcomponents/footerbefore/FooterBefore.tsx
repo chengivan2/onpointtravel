3d097c83154db0a93ecad6892c120b79abc3ef5b
chengivan <chengoleivan@gmail.com>
2025-04-02 22:15:18 +0300

    Modified the footerbefore

--- src/app/rootcomponents/footerbefore/FooterBefore.tsx
+++ src/app/rootcomponents/footerbefore/FooterBefore.tsx
@@ -2,12 +2,18 @@ import FooterSubscribe from "./FooterSubscribe";
 
 export default function FooterBefore() {
   return (
-    <section className="relative flex justify-center min-h-[80vh] min-w-full px-3 py-2">
+    <section className="relative flex justify-center min-h-[60vh] min-w-full px-3 py-2">
       <div className="min-w-full h-[30vh] flex flex-col-reverse md:flex-row bg-lightmode-footer-bg-color/80 dark:bg-darkmode-footer-bg-color/80 rounded-[8px] lg:rounded-[16px] backdrop-blur-md border">
-        <div className="relative min-h-full md:w-[50%] bg-[url('https://res.cloudinary.com/doqbnfais/image/upload/v1743619051/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/before%20footer%20images/newsletter%20featured%20images/1_plilsj.png')] dark:bg-[url('https://res.cloudinary.com/doqbnfais/image/upload/v1743619052/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/before%20footer%20images/newsletter%20featured%20images/2_ixo865.png')] bg-cover bg-center bg-no-repeat flex items-center justify-center"></div>
-        <div className="relative min-w-full md:w-[50%] min-h-full">
+
+        <div className="relative min-h-full md:w-[50%] flex items-center justify-center p-2">
+          <div className="relative min-w-full min-h-full rounded-[8px] bg-[url('https://res.cloudinary.com/doqbnfais/image/upload/v1743619051/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/before%20footer%20images/newsletter%20featured%20images/1_plilsj.png')] dark:bg-[url('https://res.cloudinary.com/doqbnfais/image/upload/v1743619052/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/before%20footer%20images/newsletter%20featured%20images/2_ixo865.png')] bg-cover bg-center bg-no-repeat">
+          </div>
+        </div>
+
+        <div className="relative min-w-full md:w-[50%] min-h-full p-2 flex items-center justify-center">
           <FooterSubscribe />
         </div>
+
       </div>
     </section>
   );
