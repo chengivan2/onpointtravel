69c44d41fbe305b68a77e05ca6be257f9972f895
chengivan <chengoleivan@gmail.com>
2025-04-02 16:25:03 +0300

    Added layout for the destinations page

---
+++ src/app/destinations/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { inter, alegreyaSans } from "@/app/fonts";
+import { ThemeProvider } from "next-themes";
+import "./globals.css";
+import ScrollToTop from "@/app/rootcomponents/ScrollToTop";
+
+export const metadata: Metadata = {
+  title: "OnPoint Travel Destinations",
+  description: "Discover different destinations",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable} ${alegreyaSans}`}>
+      <body className={`antialiased min-h-[100vh] flex flex-col bg-lightmode-bg-color dark:bg-darkmode-bg-color`}>
+        <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+          {children}
+        </ThemeProvider>
+        <ScrollToTop />
+      </body>
+    </html>
+  );
+}
