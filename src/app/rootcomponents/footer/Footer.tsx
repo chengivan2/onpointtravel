b4f6890417af77b42734500854abf7c6572ae1f3
chengivan <chengoleivan@gmail.com>
2025-04-02 21:42:44 +0300

    Modified the footer's top margin

--- src/app/rootcomponents/footer/Footer.tsx
+++ src/app/rootcomponents/footer/Footer.tsx
@@ -5,7 +5,7 @@ import Footersocials from "./FooterSocials";
 
 export default function Footer() {
   return (
-    <footer className="bg-lightmode-footer-bg-color pt-20 dark:bg-darkmode-footer-bg-color">
+    <footer className="bg-lightmode-footer-bg-color pt-20 dark:bg-darkmode-footer-bg-color mt-8">
       <div className="">
         <div className="px-[2rem] grid gap-12 md:grid-cols-5">
           <div className="md:col-span-2">
