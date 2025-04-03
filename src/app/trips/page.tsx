5f466e48dfc262f2022bd87baf5f058c1bb68044
chengivan <chengoleivan@gmail.com>
2025-04-03 19:28:11 +0300

    Trips page created and individual trips being displayedproperly together with their pages

--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -1,8 +1,10 @@
-
 import { createClient } from "@/utils/supabase/server";
 import { SearchBar } from "./components/SearchBar";
 import TripCard from "./components/TripCard";
 import { Database } from "@/types/supabase";
+import Header from "../rootcomponents/header/Header";
+import FooterBefore from "../rootcomponents/footerbefore/FooterBefore";
+import Footer from "../rootcomponents/footer/Footer";
 
 export default async function TripsPage({
   searchParams,
@@ -12,11 +14,7 @@ export default async function TripsPage({
   const supabase = await createClient();
   const searchTerm = (await searchParams)?.query || "";
 
-  let query = supabase
-    .from("trips")
-    .select(
-      "*"
-    );
+  let query = supabase.from("trips").select("*");
 
   if (searchTerm) {
     query = query
@@ -27,18 +25,23 @@ export default async function TripsPage({
   const { data: trips } = await query;
 
   return (
-    <div className="min-h-screen bg-green-50/20 dark:bg-green-900/10">
-      <div className="max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8">
-        <div className="mb-12">
-          <SearchBar placeholder="Search trips..." />
-        </div>
+    <>
+      <Header />
+      <div className="min-h-screen bg-green-50/20 dark:bg-green-900/10">
+        <div className="max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8">
+          <div className="mb-12">
+            <SearchBar placeholder="Search trips..." />
+          </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {trips?.map((trip) => (
-            <TripCard key={trip.id} trip={trip} />
-          ))}
+          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {trips?.map((trip) => (
+              <TripCard key={trip.id} trip={trip} />
+            ))}
+          </section>
         </div>
       </div>
-    </div>
+      <FooterBefore />
+      <Footer />
+    </>
   );
 }
