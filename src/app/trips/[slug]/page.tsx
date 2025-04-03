fe95680d5aeaba58174aedec0c1f7ac65720573a
chengivan <chengoleivan@gmail.com>
2025-04-03 21:21:43 +0300

    Usintg inline styles to set background image to the hero section of the single trips page

--- src/app/trips/[slug]/page.tsx
+++ src/app/trips/[slug]/page.tsx
@@ -89,7 +89,8 @@ export default async function TripPage({
 
         {/* Image Gallery */}
         <div
-          className={`relative min-w-full min-h-[100vh] bg-cover bg-center bg-[url(\'${mainImage}\')] rounded-xl overflow-hidden mb-12`}
+          style={{ backgroundImage: `url(${mainImage})` }}
+          className="relative min-w-full min-h-[100vh] bg-cover bg-center rounded-xl overflow-hidden mb-12"
         ></div>
 
         {/* Overview Section */}
