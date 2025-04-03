139a7295a14a012c5f6a1401df8c4970ef71e0e3
chengivan <chengoleivan@gmail.com>
2025-04-03 04:28:23 +0300

    FooterSubscribe form covers full width

--- src/app/rootcomponents/footerbefore/FooterSubscribe.tsx
+++ src/app/rootcomponents/footerbefore/FooterSubscribe.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function FooterSubscribe() {
   return (
-    <form className="w-[50%] mt-12">
+    <form className="min-w-full mt-12">
       <div className="space-y-2.5">
         <Label className="block text-sm font-medium" htmlFor="email">
           Subscribe to our newsletter
