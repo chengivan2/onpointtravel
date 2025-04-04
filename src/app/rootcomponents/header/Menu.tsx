1fc45c3daeb10457dbde0d52d6f4f94fb0874c16
chengivan <chengoleivan@gmail.com>
2025-04-04 13:30:05 +0300

    Added trips link to the header

--- src/app/rootcomponents/header/Menu.tsx
+++ src/app/rootcomponents/header/Menu.tsx
@@ -5,16 +5,34 @@ import Link from "next/link";
 export default function Menu() {
   return (
     <div className="text-[0.85rem] lg:flex space-y-3 lg:space-y-0 lg:space-x-8">
-      <Link href="/" className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block">
+      <Link
+        href="/"
+        className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block"
+      >
         Home
       </Link>
-      <Link href="/destinations" className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block">
+      <Link
+        href="/destinations"
+        className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block"
+      >
         Destinations
       </Link>
-      <Link href="#" className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block">
+      <Link
+        href="/trips"
+        className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block"
+      >
+        Trips
+      </Link>
+      <Link
+        href="#"
+        className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block"
+      >
         About
       </Link>
-      <Link href="#" className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block">
+      <Link
+        href="#"
+        className="text-lightmode-headertext-color dark:text-darkmode-headertext-color hover:text-lightmode-headertext-hover-color dark:hover:text-darkmode-headertext-hover-color active:text-lightmode-headertext-active-color dark:active:text-darkmode-headertext-active-color block"
+      >
         Contact
       </Link>
     </div>
