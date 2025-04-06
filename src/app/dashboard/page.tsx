51044d031984444a7226921ef5aea754d81a002e
chengivan <chengoleivan@gmail.com>
2025-04-06 13:34:30 +0300

    Modified the dashboard to redirect non-loggerd in users to login page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function OnPointDashboard() {
-  return (
-    <div>onPoint user dashboard</div>
-  )
+export default async function OnPointDashboard() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    redirect("/login?next=/dashboard");
+  }
+
+  return <div>onPoint user dashboard</div>;
 }
