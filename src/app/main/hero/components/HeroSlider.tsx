b4022f63840e2c84a624eb6204fa0637697486f2
chengivan <chengoleivan@gmail.com>
2025-04-08 21:22:22 +0300

    Added the HeroSlider component

---
+++ src/app/main/hero/components/HeroSlider.tsx
@@ -0,0 +1,12 @@
+import { createClient } from "@/utils/supabase/server";
+import { Database } from "@/types/supabase";
+
+type Trips = Database["public"]["Tables"]["trips"]["Row"];
+
+export default function HeroSlider() {
+  return (
+    <section>
+
+    </section>
+  )
+}
