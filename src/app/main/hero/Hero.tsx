c13d638e31618cc9de13e9a343bb9e485b4682b1
chengivan <chengoleivan@gmail.com>
2025-04-08 22:50:29 +0300

    Removed home hero section empty wrappers

--- src/app/main/hero/Hero.tsx
+++ src/app/main/hero/Hero.tsx
@@ -1,17 +1,9 @@
-import AnimatedHeading from "./AnimatedHeading";
 import HeroSlider from "./components/HeroSlider";
-
 
 export default function HomeHero() {
   return (
-    <>
     <section className="relative bg-transparent px-[0.8rem] lg:px-[1rem] mt-20 py-[0.8rem] lg:py-[2rem] min-w-full min-h-[100vh]">
-
       <HeroSlider />
-
     </section>
-    </>
-
-
   );
 }
