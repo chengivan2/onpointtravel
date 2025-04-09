f409e23f6027b9853e6df0eb7744555a694fe334
chengivan <chengoleivan@gmail.com>
2025-04-09 22:18:53 +0300

    Moved the Make your own trip button to CTAOne and displayed CTAOne on HeaderButtons

--- src/app/rootcomponents/header/HeaderButtons.tsx
+++ src/app/rootcomponents/header/HeaderButtons.tsx
@@ -1,22 +1,10 @@
-
-import React from "react";
-import Link from "next/link";
 import HeaderAuthButton from "./HeaderAuthButton";
+import CTAOne from "./CTAOne";
 
 export default function HeaderButtons() {
   return (
     <>
-      <Link href="/" className="min-w-full lg:min-w-auto flex flex-row justify-center items-center px-4 py-2 rounded-full text-sm font-medium
-                     bg-lightmode-btn-bg-color text-lightmode-btn-text-color
-                     hover:bg-lightmode-btn-bg-hover-color
-                     dark:bg-darkmode-btn-bg-color dark:text-darkmode-btn-text-color
-                     dark:hover:bg-darkmode-btn-bg-hover-color
-                     transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lightmode-heading-color dark:focus:ring-darkmode-heading-color focus:ring-offset-lightmode-header-bg-color dark:focus:ring-offset-darkmode-header-bg-color"
-        >
-          Make your own trip
-        
-      </Link>
-
+      <CTAOne />
       <HeaderAuthButton />
     </>
   );
