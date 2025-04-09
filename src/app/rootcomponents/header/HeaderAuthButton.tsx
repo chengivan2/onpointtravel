4760d265d3556c935bcfa6373043b663444b7302
chengivan <chengoleivan@gmail.com>
2025-04-09 17:53:57 +0300

    Modified header buttons

--- src/app/rootcomponents/header/HeaderAuthButton.tsx
+++ src/app/rootcomponents/header/HeaderAuthButton.tsx
@@ -26,7 +26,7 @@ export default function HeaderAuthButton() {
   return user ? (
     <Link
       href="/dashboard"
-      className="px-4 flex flex-row gap-[0.8rem] border border-green-500 justify-center items-center py-2 rounded-full text-sm font-medium border-none text-lightmode-secondary-btn-text-color hover:bg-lightmode-secondary-btn-bg-hover-color hover:text-lightmode-secondary-btn-text-hover-color dark:border-darkmode-secondary-btn-border-color dark:text-darkmode-secondary-btn-text-color dark:hover:bg-darkmode-secondary-btn-bg-hover-color dark:hover:text-darkmode-secondary-btn-text-hover-color dark:hover:border-darkmode-secondary-btn-border-hover-color transition-colors focus:outline-none"
+      className="min-w-full px-4 flex flex-row gap-[0.8rem] border border-green-500 justify-center items-center py-2 rounded-full text-sm font-medium text-lightmode-secondary-btn-text-color hover:bg-lightmode-secondary-btn-bg-hover-color hover:text-lightmode-secondary-btn-text-hover-color dark:border-darkmode-secondary-btn-border-color dark:text-darkmode-secondary-btn-text-color dark:hover:bg-darkmode-secondary-btn-bg-hover-color dark:hover:text-darkmode-secondary-btn-text-hover-color dark:hover:border-darkmode-secondary-btn-border-hover-color transition-colors focus:outline-none"
     >
       <FiUser size="18" />
       Dashboard
--- src/app/rootcomponents/header/HeaderButtons.tsx
+++ src/app/rootcomponents/header/HeaderButtons.tsx
@@ -5,10 +5,8 @@ import HeaderAuthButton from "./HeaderAuthButton";
 
 export default function HeaderButtons() {
   return (
-    <div className="min-w-full flex flex-col md:flex-row items-center space-x-3">
-      <Link href="/">
-        <a
-          className="px-4 py-2 rounded-full text-sm font-medium
+    <div className="min-w-full flex flex-col md:flex-row items-center gap-[1rem] space-x-3">
+      <Link href="/" className="min-w-full px-4 py-2 rounded-full text-sm font-medium
                      bg-lightmode-btn-bg-color text-lightmode-btn-text-color
                      hover:bg-lightmode-btn-bg-hover-color
                      dark:bg-darkmode-btn-bg-color dark:text-darkmode-btn-text-color
@@ -16,7 +14,7 @@ export default function HeaderButtons() {
                      transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lightmode-heading-color dark:focus:ring-darkmode-heading-color focus:ring-offset-lightmode-header-bg-color dark:focus:ring-offset-darkmode-header-bg-color"
         >
           Make your own trip
-        </a>
+        
       </Link>
 
       <HeaderAuthButton />
