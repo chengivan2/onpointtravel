4658fa12f048380a45da83e6fc95d80dbccfe77a
chengivan <chengoleivan@gmail.com>
2025-04-09 22:03:17 +0300

    Modified the header buttons

--- src/app/rootcomponents/header/Header.tsx
+++ src/app/rootcomponents/header/Header.tsx
@@ -111,11 +111,11 @@ export default function Header() {
 
       {/* Mobile Navigation */}
       {isMenuOpen && (
-        <div className="flex flex-col lg:hidden py-4 px-4 h-auto space-y-3 absolute top-full left-0 right-0 bg-lightmode-header-bg-color dark:bg-darkmode-bg-color border-t dark:border-gray-700">
+        <div className="flex flex-col lg:hidden py-4 px-4 h-auto space-y-3 absolute top-full left-0 right-0 bg-lightmode-header-bg-color dark:bg-darkmode-bg-color border-t dark:border-green-900">
           <nav className="">
             <Menu />
           </nav>
-          <div className="flex flex-col justify-center items-center gap-[1rem] space-x-4 pt-3 border-t dark:border-gray-700">
+          <div className="flex flex-col justify-center items-center gap-[1rem] pt-3 border-t dark:border-green-900">
             <HeaderButtons />
           </div>
         </div>
