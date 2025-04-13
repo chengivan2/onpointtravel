ca87f7e6efd07a64878af85384ed58205d900175
chengivan <chengoleivan@gmail.com>
2025-04-13 22:29:24 +0300

    Modified the first admin dashboard card styling

--- src/app/dashboard/components/DataSectionCards.tsx
+++ src/app/dashboard/components/DataSectionCards.tsx
@@ -106,7 +106,7 @@ export default async function DataSectionCards() {
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       {profile?.role === "admin" && (
-        <Card className="@container/card">
+        <Card className="@container/card bg-white/60 dark:bg-green-900/20 backdrop-blur-md border border-green-100/30 dark:border-green-900/30">
           <CardHeader>
             <CardDescription>Total Bookings</CardDescription>
             <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
