a45eaaa5223344fdc612fbc70e1853c9b4fe6065
chengivan <chengoleivan@gmail.com>
2025-04-14 17:20:00 +0300

    Modified the dashboard sidebar bg color

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -244,7 +244,7 @@ function Sidebar({
         <div
           data-sidebar="sidebar"
           data-slot="sidebar-inner"
-          className="bg-white/60 dark:bg-green-900/20 border-green-900/30 group-data-[variant=floating]:border-sidebar-border flex h-full w-full flex-col group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:shadow-sm"
+          className="bg-white/800 dark:bg-green-900/40 border-green-900/30 group-data-[variant=floating]:border-sidebar-border flex h-full w-full flex-col group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:shadow-sm"
         >
           {children}
         </div>
