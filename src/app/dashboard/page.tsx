d432eb26c518568f75d4959416d89f96eac9e0a2
chengivan <chengoleivan@gmail.com>
2025-04-14 11:45:49 +0300

    Protected the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,29 @@
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { DataTable } from "@/components/data-table"
-import { SiteHeader } from "@/components/site-header"
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
-
-import data from "./data.json"
-import { Metadata } from "next"
-import DataSectionCards from "./components/DataSectionCards"
-import { DashboardSidebar } from "./components/sidebar/DashboardSideBar"
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { createClient } from "@/utils/supabase/server";
+import data from "./data.json";
+import { Metadata } from "next";
+import { DashboardSidebar } from "./components/sidebar/DashboardSideBar";
+import AdminDataSlotCards from "./components/admincomponents/AdminDataSlotCards";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "OnPoint Dashboard",
   description: "Manage your OnPoint account",
 };
 
-
 export default async function OnPointDashboard() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/signin");
+  }
   return (
     <SidebarProvider
       style={
@@ -33,7 +39,7 @@ export default async function OnPointDashboard() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <DataSectionCards />
+              <AdminDataSlotCards />
               <div className="px-4 lg:px-6">
                 <ChartAreaInteractive />
               </div>
@@ -43,5 +49,5 @@ export default async function OnPointDashboard() {
         </div>
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
