e2243fececdcea6a3f94c2148cd39e894b385f2a
chengivan <chengoleivan@gmail.com>
2025-04-15 22:38:33 +0300

    Modified the mobile sidebar bg

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -40,6 +40,9 @@ export default async function OnPointDashboard() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <div className="relative min-w-full ">
+                <h2>Hello </h2>
+              </div>
               <AdminDataSlotCards />
               <div className="px-4 lg:px-6">
                 <AdminBookingsChart />
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -187,7 +187,7 @@ function Sidebar({
           data-sidebar="sidebar"
           data-slot="sidebar"
           data-mobile="true"
-          className="text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
+          className="bg-green-900/80 backdrop-blur-[2px] text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
           style={
             {
               "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
