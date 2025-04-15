9dd33bd8056243d5b4a129ca01ccd033765f01ec
chengivan <chengoleivan@gmail.com>
2025-04-15 11:54:14 +0300

    Modified the adminschart styling

--- src/app/dashboard/components/admincomponents/AdminBookingsChart.tsx
+++ src/app/dashboard/components/admincomponents/AdminBookingsChart.tsx
@@ -63,7 +63,7 @@ export default function AdminBookingsChart() {
   }, [supabase]);
 
   return (
-    <Card className="bg-green-800 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100">
+    <Card className="bg-white/80 dark:bg-green-900/40 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100 dark:border-green-900">
       <CardHeader>
         <CardTitle>Monthly Bookings</CardTitle>
         <CardDescription>
