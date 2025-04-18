1607b97c95a232c736752eb93a8c427832c48bec
chengivan <chengoleivan@gmail.com>
2025-04-18 22:49:46 +0300

    Wrapped the whole dashboard sidebar items in Links containing their urls

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -45,14 +45,14 @@ export function NavMain({
         </SidebarMenu>
         <SidebarMenu>
           {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <Link href={item.url}>
+            <Link href={item.url}>
+              <SidebarMenuItem key={item.title}>
                 <SidebarMenuButton tooltip={item.title}>
                   {item.icon && <item.icon />}
                   <span>{item.title}</span>
                 </SidebarMenuButton>
-              </Link>
-            </SidebarMenuItem>
+              </SidebarMenuItem>
+            </Link>
           ))}
         </SidebarMenu>
       </SidebarGroupContent>
