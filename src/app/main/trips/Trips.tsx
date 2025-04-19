6094477f769e71af4c11378f1d9854e74890a8ab
chengivan <chengoleivan@gmail.com>
2025-04-19 08:35:00 +0300

    Added new blob svg as bg on home top trips

--- src/app/main/trips/Trips.tsx
+++ src/app/main/trips/Trips.tsx
@@ -6,41 +6,56 @@ export default function Trips() {
     <section className="py-[3rem] bg-green-50/20 dark:bg-green-900/10 relative flex flex-col min-w-full">
       
       <div className="absolute inset-0 -z-10">
-        <svg
-          className="absolute top-0 left-0 w-[30rem] h-[30rem] opacity-20"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 200 200"
-        >
-          <path
-            fill="url(#gradient1)"
-            d="M40.5,-60.4C52.5,-50.3,61.5,-39.2,66.8,-26.8C72.1,-14.4,73.7,-0.8,70.6,11.1C67.5,23,59.7,33.2,50.1,42.1C40.5,51,29.1,58.6,16.6,62.6C4.1,66.6,-9.5,67,-22.4,62.6C-35.3,58.2,-47.5,48.9,-55.5,37.1C-63.5,25.3,-67.3,11,-67.4,-3.6C-67.5,-18.2,-63.9,-33.1,-55.3,-44.5C-46.7,-55.9,-33.1,-63.8,-19.1,-68.1C-5.1,-72.4,9.3,-73.1,22.7,-69.8C36.1,-66.5,48.5,-59.2,40.5,-60.4Z"
-            transform="translate(100 100)"
-          />
-          <defs>
-            <linearGradient id="gradient1" x1="0" x2="1" y1="0" y2="1">
-              <stop offset="0%" stopColor="#34D399" />
-              <stop offset="100%" stopColor="#60A5FA" />
-            </linearGradient>
-          </defs>
-        </svg>
+        
+        <svg className="absolute top-40 left-30 w-[30rem] h-[30rem] opacity-20" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <radialGradient id="greenGradient" cx="50%" cy="50%" r="50%">
+      <stop offset="0%" stop-color="#4caf50" />
+      <stop offset="100%" stop-color="#2e7d32" />
+    </radialGradient>
+  </defs>
 
-        <svg
-          className="absolute bottom-0 right-0 w-[20rem] h-[20rem] opacity-30"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 200 200"
-        >
-          <path
-            fill="url(#gradient2)"
-            d="M34.5,-47.8C45.7,-37.8,54.3,-28.5,60.4,-16.8C66.5,-5.1,70.1,9,66.1,20.6C62.1,32.2,50.5,41.3,38.1,48.7C25.7,56.1,12.8,61.8,-0.3,62.2C-13.4,62.6,-26.8,57.7,-38.2,50.2C-49.6,42.7,-59,32.6,-63.4,20.8C-67.8,9,-67.2,-4.5,-61.8,-16.5C-56.4,-28.5,-46.2,-39,-34.7,-49.1C-23.2,-59.2,-11.6,-68.8,0.3,-69.1C12.2,-69.4,24.4,-60.5,34.5,-47.8Z"
-            transform="translate(100 100)"
-          />
-          <defs>
-            <linearGradient id="gradient2" x1="0" x2="1" y1="0" y2="1">
-              <stop offset="0%" stopColor="#FBBF24" />
-              <stop offset="100%" stopColor="#F87171" />
-            </linearGradient>
-          </defs>
-        </svg>
+  <g fill="url(#greenGradient)">
+    
+    <path d="M180,120 
+             C240,60 360,60 420,120 
+             C480,180 480,300 420,360 
+             C360,420 240,420 180,360 
+             C120,300 120,180 180,120 
+             Z 
+             M220,180 
+             C260,140 340,140 380,180 
+             C420,220 420,280 380,320 
+             C340,360 260,360 220,320 
+             C180,280 180,220 220,180 
+             Z" />
+
+  
+    <path d="M120,400 
+             C160,360 240,360 280,400 
+             C320,440 320,520 280,560 
+             C240,600 160,600 120,560 
+             C80,520 80,440 120,400 
+             Z" />
+
+  
+    <path d="M300,300 
+             C350,250 450,250 500,300 
+             C550,350 550,450 500,500 
+             C450,550 350,550 300,500 
+             C250,450 250,350 300,300 
+             Z" />
+
+    
+    <path d="M400,100 
+             C500,120 550,200 520,300 
+             C490,400 420,480 350,440 
+             C280,400 300,300 330,240 
+             C360,180 330,80 400,100 
+             Z" />
+  </g>
+</svg>
+
       </div>
 
       <div className="flex relative min-w-full justify-center">
