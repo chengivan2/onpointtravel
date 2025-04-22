1c8932256ac9ef7374a131356784928bec87fd89
chengivan <chengoleivan@gmail.com>
2025-04-22 11:19:34 +0300

    Removed the home destinations bg and deleted its file. Last was a mistake where I removed the DestinationCards instead of the home's destination section's background. My bad.

--- src/app/main/destinations/Destinations.tsx
+++ src/app/main/destinations/Destinations.tsx
@@ -3,13 +3,12 @@ import DestinationCards from "./DestinationCards";
 export default function Destinations() {
   return (
     <section className="relative flex flex-col min-w-full">
-     <DestinationsSectionBg />
       <div className="flex relative min-w-full justify-center">
         <h2 className="text-4xl font-black text-lightmode-heading-color dark:text-darkmode-heading-color">
           Top Destinations
         </h2>
       </div>
-
+      <DestinationCards />
     </section>
   );
 }
