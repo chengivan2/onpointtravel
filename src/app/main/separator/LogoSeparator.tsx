3fcc3fc7c5f650e8aaf5317fade9311e4fa65fec
chengivan <chengoleivan@gmail.com>
2025-04-28 14:36:45 +0300

    Created the LogoSeparator component

---
+++ src/app/main/separator/LogoSeparator.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Image from "next/image";
+
+export default function LogoSeparator() {
+  return (
+    <div className="flex items-center justify-center w-full my-8">
+      {/* Left Line */}
+      <div className="flex-1 h-[1px] bg-lightmode-text-color dark:bg-darkmode-text-color opacity-50"></div>
+
+      {/* Glowing Logo */}
+      <div className="mx-4 relative">
+        <div className="relative z-10">
+          <Image
+            src="/logos/onpointhflightmodelogo.png"
+            alt="Logo"
+            width={40} 
+            height={40}
+            className="dark:hidden"
+          />
+        </div>
+        <div className="absolute inset-0 blur-md bg-green-500 rounded-full opacity-50"></div>
+      </div>
+
+      {/* Right Line */}
+      <div className="flex-1 h-[1px] bg-lightmode-text-color dark:bg-darkmode-text-color opacity-50"></div>
+    </div>
+  );
+}
