6280d014d0dfc143a08e64b931993b34b4a2aacb
chengivan <chengoleivan@gmail.com>
2025-04-28 11:38:21 +0300

    Modified the stats section's texts colors

--- src/app/main/stats/Stats.tsx
+++ src/app/main/stats/Stats.tsx
@@ -4,13 +4,13 @@ import SlotCounter from "react-slot-counter";
 
 export default function Stats() {
   return (
-    <section className="py-12 md:py-20">
+    <section className="pt-6 md:pt-10 py-12 md:py-20">
       <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
         <div className="relative z-10 mx-auto max-w-xl space-y-6 text-center">
-          <h2 className="text-4xl font-semibold lg:text-5xl">
+          <h2 className="text-lightmode-heading-color dark:text-darkmode-heading-color text-4xl font-semibold lg:text-5xl">
             Straight to the onPoint Stats
           </h2>
-          <p>
+          <p className="text-lightmode-text-color dark:text-darkmode-text-color">
             With our reach and contacts all over the world, getting you from
             point A to point B is way easier and much more comfy with onPoint.
             You might think it's a lie but numbers never lie
