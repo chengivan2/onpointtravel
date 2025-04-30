0bb0f2d1eadefb480dd151abb67192b6ec29f765
chengivan <chengoleivan@gmail.com>
2025-04-30 14:50:34 +0300

    Formatted the extra trp offer component

--- src/app/main/extratripoffer/ExtraTripOffer.tsx
+++ src/app/main/extratripoffer/ExtraTripOffer.tsx
@@ -7,42 +7,47 @@ export default function ExtraTripOffer() {
   return (
     <section className="relative">
       <div
-      className="absolute inset-0 bg-cover bg-center opacity-20"
-      style={{
-        backgroundImage:
-        "url('https://res.cloudinary.com/doqbnfais/image/upload/v1745260620/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/three-impalas_kwx3j6.jpg')",
-      }}
+        className="absolute inset-0 bg-cover bg-center opacity-20"
+        style={{
+          backgroundImage:
+            "url('https://res.cloudinary.com/doqbnfais/image/upload/v1745260620/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/three-impalas_kwx3j6.jpg')",
+        }}
       ></div>
       <div className="relative bg-transparent py-12 md:py-24">
-      <div className="mx-auto max-w-5xl px-6">
-        <div className="mx-auto max-w-md px-6 [mask-image:radial-gradient(ellipse_100%_100%_at_50%_0%,#0F0_70%,transparent_100%)]">
-        <div className="bg-white/60 dark:bg-green-900/50 rounded-xl backdrop-blur-md border p-3 shadow-xl">
-          <Image
-          src="https://res.cloudinary.com/doqbnfais/image/upload/v1745260620/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/three-impalas_kwx3j6.jpg"
-          alt="Three gazelles staring"
-          width={600}
-          height={600}
-          className="rounded-lg"
-          />
-        </div>
-        </div>
-        <div className="mx-auto mt-6 max-w-lg space-y-6 text-center">
-        <h2 className="text-balance text-lightmode-heading-color dark:text-darkmode-heading-color text-3xl font-semibold md:text-4xl lg:text-5xl">
-          Play even harder
-        </h2>
-        <p className="md:text-md text-lightmode-text-color dark:text-darkmode-text-color">
-          Book three trips in a year and get a 27% discount on the next one.
-        </p>
+        <div className="mx-auto max-w-5xl px-6">
+          <div className="mx-auto max-w-md px-6 [mask-image:radial-gradient(ellipse_100%_100%_at_50%_0%,#0F0_70%,transparent_100%)]">
+            <div className="bg-white/60 dark:bg-green-900/50 rounded-xl backdrop-blur-md border p-3 shadow-xl">
+              <Image
+                src="https://res.cloudinary.com/doqbnfais/image/upload/v1745260620/onPoint%20website%20concept/website%20assets/website%20images/website%20design%20and%20stock%20photos/three-impalas_kwx3j6.jpg"
+                alt="Three gazelles staring"
+                width={600}
+                height={600}
+                className="rounded-lg"
+              />
+            </div>
+          </div>
+          <div className="mx-auto mt-6 max-w-lg space-y-6 text-center">
+            <h2 className="text-balance text-lightmode-heading-color dark:text-darkmode-heading-color text-3xl font-semibold md:text-4xl lg:text-5xl">
+              Play even harder
+            </h2>
+            <p className="md:text-md text-lightmode-text-color dark:text-darkmode-text-color">
+              Book three trips in a year and get a 27% discount on the next one.
+            </p>
 
-        <Button
-          className="bg-lightmode-btn-bg-color hover:bg-lightmode-btn-bg-hover-color dark:bg-darkmode-btn-bg-color hover:dark:bg-darkmode-btn-bg-hover-color shadow-2xl"
-          size="lg"
-          asChild
-        >
-          <Link className="text-lightmode-text-color dark:text-darkmode-text-color md:text-md" href="/trips">Book a Trip</Link>
-        </Button>
+            <Button
+              className="bg-lightmode-btn-bg-color hover:bg-lightmode-btn-bg-hover-color dark:bg-darkmode-btn-bg-color hover:dark:bg-darkmode-btn-bg-hover-color shadow-2xl"
+              size="lg"
+              asChild
+            >
+              <Link
+                className="text-lightmode-text-color dark:text-darkmode-text-color md:text-md"
+                href="/trips"
+              >
+                Book a Trip
+              </Link>
+            </Button>
+          </div>
         </div>
-      </div>
       </div>
     </section>
   );
