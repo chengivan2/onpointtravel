b46cce292c9df8de874e4530c296fe96f8403cdd
chengivan <chengoleivan@gmail.com>
2025-05-15 07:35:53 +0300

    Modified the FooterLogo

--- src/app/rootcomponents/footer/FooterLogo.tsx
+++ src/app/rootcomponents/footer/FooterLogo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function FooterLogo() {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-center items-center">
       <Image
         width={100}
         height={50}
