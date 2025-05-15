b4140a778612b4feb0c2338af112648310b2a319
chengivan <chengoleivan@gmail.com>
2025-05-15 07:31:54 +0300

    Modified the footer section styling

--- src/app/rootcomponents/footer/Footer.tsx
+++ src/app/rootcomponents/footer/Footer.tsx
@@ -5,9 +5,9 @@ import Footersocials from "./FooterSocials";
 
 export default function Footer() {
   return (
-    <footer className="min-w-full h-[60vh] bg-lightmode-footer-bg-color p-16 dark:bg-darkmode-footer-bg-color mt-12">
-      <div className="relative flex flex-col gap-[1rem] rounded-lg bg-[url(/images/footer.png)] bg-cover bg-no-repeat bg-bottom h-full w-full px-4 py-3">
-        <div className="absolute inset-0 bg-[#FAFAFA] dark:bg-[#181818] opacity-30 dark:opacity-40 rounded-lg"></div>
+    <footer className="min-w-full bg-lightmode-footer-bg-color p-16 dark:bg-darkmode-footer-bg-color mt-12">
+      <div className="relative flex flex-col gap-[1rem] rounded-lg bg-[url(/images/footer.png)] bg-cover bg-no-repeat bg-bottom h-full w-full px-2 py-2 md:px-6 md:py-6 lg:px-10 lg:py-10">
+        <div className="absolute inset-0 bg-[#181818] opacity-50 rounded-lg"></div>
         <div className="relative min-w-full px-[2rem] grid gap-12 md:grid-cols-5">
           <div className="md:col-span-2">
             <Link href="/" aria-label="go home" className="block size-fit">
