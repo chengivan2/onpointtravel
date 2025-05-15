f9c4c863d463643be281f5cb25694341a6ce5d0c
chengivan <chengoleivan@gmail.com>
2025-05-15 07:25:35 +0300

    Modified the footer section styling

--- src/app/rootcomponents/footer/Footer.tsx
+++ src/app/rootcomponents/footer/Footer.tsx
@@ -6,9 +6,9 @@ import Footersocials from "./FooterSocials";
 export default function Footer() {
   return (
     <footer className="min-w-full h-[60vh] bg-lightmode-footer-bg-color p-16 dark:bg-darkmode-footer-bg-color mt-12">
-      <div className="relative rounded-lg bg-[url(/images/footer.png)] bg-cover bg-no-repeat bg-bottom h-full w-full px-4 py-3">
+      <div className="relative flex flex-col gap-[1rem] rounded-lg bg-[url(/images/footer.png)] bg-cover bg-no-repeat bg-bottom h-full w-full px-4 py-3">
         <div className="absolute inset-0 bg-[#FAFAFA] dark:bg-[#181818] opacity-30 dark:opacity-40 rounded-lg"></div>
-        <div className="px-[2rem] grid gap-12 md:grid-cols-5">
+        <div className="relative min-w-full px-[2rem] grid gap-12 md:grid-cols-5">
           <div className="md:col-span-2">
             <Link href="/" aria-label="go home" className="block size-fit">
               <FooterLogo />
@@ -17,7 +17,7 @@ export default function Footer() {
 
           <FooterMenu />
         </div>
-        <div className="px-[2rem] lg:px-[2rem] flex flex-wrap items-end justify-between gap-6 border-t dark:border-t py-6">
+        <div className="relative min-w-full px-[2rem] lg:px-[2rem] flex flex-wrap items-end justify-between gap-6 border-t dark:border-t py-6">
           <span className="text-muted-foreground order-last block text-center text-sm md:order-first">
             © {new Date().getFullYear()} OnPoint Travel Agency
           </span>
