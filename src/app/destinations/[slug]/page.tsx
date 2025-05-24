7ddebfec4d7e8d69dfd54b1634e318966b191c73
chengivan <chengoleivan@gmail.com>
2025-05-24 16:09:04 +0800

    Single destination page hero location area modified

--- src/app/destinations/[slug]/page.tsx
+++ src/app/destinations/[slug]/page.tsx
@@ -55,7 +55,7 @@ export default async function DestinationPage({
                 {destination.name}
               </h1>
               
-              <span className="flex flex-row flex-wrap gap-2 mt-4 text-green-700 dark:text-green-300">
+              <span className="w-full flex flex-row flex-wrap items-center gap-2 mt-4 text-green-700 dark:text-green-300">
                 <FaLocationArrow className="inline-block text-green-600 dark:text-green-300 mr-2" />
               <p className="text-xl text-green-600 dark:text-green-300 mt-2">
                 {destination.location}
