93a4746e2665edf8095c26572117a0376c719b87
chengivan <chengoleivan@gmail.com>
2025-05-25 20:08:35 +0300

    Modified the destination page's DestinationCard heading colors

--- src/app/destinations/components/DestinationCard.tsx
+++ src/app/destinations/components/DestinationCard.tsx
@@ -32,7 +32,7 @@ export default function DestinationCard({
         <p className="text-sm text-lightmode-text-color/80 dark:text-darkmode-text-color/80">
           {destination.location}
         </p>
-        <p className="text-lightmode-text-color/90 dark:text-darkmode-text-color/90 text-base line-clamp-3">
+        <p className="text-green-800 dark:text-green-100 text-base line-clamp-3">
           {destination.description}
         </p>
       </div>
