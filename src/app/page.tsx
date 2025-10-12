a1e21b949104064d89e81c00830021dda8cae5ce
Ivan Mulindo <chengoleivan@gmail.com>
2025-10-12 20:59:27 +0300

    Added the open graph object to the home page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,20 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "OnPoint Travel",
   description: "Book trips with onPoint Travel",
+  openGraph: {
+    title: "OnPoint Travel",
+    description: "Book trips with onPoint Travel",
+    url: "https://onpointtravel.vercel.app",
+    siteName: "OnPoint Travel",
+    images: [
+      {
+        url: "https://onpoint-travel.vercel.app/og-image.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function Home() {
