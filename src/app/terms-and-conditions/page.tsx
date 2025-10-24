af3ac911cb4fca1893cc16d8027f4ec4e74d602a
Ivan Mulindo <chengoleivan@gmail.com>
2025-10-24 20:28:44 +0300

    Modified the terms and conditions' page open graph url

--- src/app/terms-and-conditions/page.tsx
+++ src/app/terms-and-conditions/page.tsx
@@ -10,7 +10,7 @@ import { Metadata } from "next";
   openGraph: {
     title: "Terms & Conditions - OnPoint Travel",
     description: "Read the terms and conditions for using OnPoint Travel services.",
-    url: "https://www.onpointtravel.com/terms-and-conditions",
+    url: "https://onpointtravel.com/terms-and-conditions",
     images: [
       {
         url: "https://res.cloudinary.com/doqbnfais/image/upload/v1760291599/onPoint%20website%20concept/OnPoint_logo_multi-use_mh0fmr.png",
