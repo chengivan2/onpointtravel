c882c0786a0db7030df624b561b9c621760c3daf
Ivan Mulindo <chengoleivan@gmail.com>
2025-10-25 12:41:05 +0300

    Added open graph details to the sign in page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -9,6 +9,18 @@ import Footer from "../rootcomponents/footer/Footer";
 export const metadata: Metadata = {
   title: "Sign In - OnPoint",
   description: "Sign in to manage your OnPoint account",
+  openGraph: {
+      title: "Sign In - OnPoint Travel",
+      description: "Sign in to your OnPoint Travel account",
+      url: "https://onpointtravel.vercel.app/signin",
+      siteName: "OnPoint Travel",
+      images: [
+        {
+          url: "https://res.cloudinary.com/doqbnfais/image/upload/v1760291599/onPoint%20website%20concept/OnPoint_logo_multi-use_mh0fmr.png",
+        },
+      ],
+      type: "website",
+    },
 };
 
 export default async function SignInPage() {
