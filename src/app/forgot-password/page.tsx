9a0903b82be49e5b22c8c4649cc205802a1977af
Ivan Mulindo <chengoleivan@gmail.com>
2025-10-28 17:19:14 +0300

    Added metadata to the forgot password page

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -2,6 +2,24 @@ import Header from "../rootcomponents/header/Header";
 import FooterBefore from "../rootcomponents/footerbefore/FooterBefore";
 import Footer from "../rootcomponents/footer/Footer";
 import ForgotPasswordForm from "./ForgotPasswordForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Forgot Password - OnPoint Travel",
+  description: "Recover your password",
+  openGraph: {
+    title: "Forgot Password - OnPoint Travel",
+    description: "Recover your password",
+    url: "https://onpointtravel.vercel.app/forgot-password",
+    siteName: "OnPoint Travel",
+    images: [
+      {
+        url: "https://res.cloudinary.com/doqbnfais/image/upload/v1760291599/onPoint%20website%20concept/OnPoint_logo_multi-use_mh0fmr.png",
+      },
+    ],
+    type: "website",
+  },
+};
 
 export default function ForgotPasswordPage() {
   return (
